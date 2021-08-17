74a2999c5d09c53aadc841d908cbd471df819523
hche0087 <hche0087@student.monash.edu>
2021-08-18 03:13:08 +1000

    fix cors

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const contractors = require('./routes/contractors');
 
 
 const app = express()
-
+const cors = require('cors');
+app.use(cors({
+    origin: 'https://hugoworkplace.xyz'
+}));
 
 app.use(express.json());
 app.use('/api/contractors', contractors);
